96fc3e7d2bbfd53624bf8ae13f8d0e1645f547c4
강창수님/SKtelecom <P152670@sktelecom.com>
2020-10-12 18:21:29 +0900

    [20.10.12][강창수] cors 처리 추가

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 app.use(cookieParser())
 app.use(express.json());
@@ -10,11 +11,19 @@ app.use(express.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const corsOptions = {
+    origin: 'http://150.20.14.142:4000', // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+app.use(cors(corsOptions)); // config 추가
+
 const cmmRoutes = require('./common/router');
+const fileUpRouter = require('./common/router/api/file');
 const apiRoutes = require('./app/router');
 const login = require('./common/router/api/auth');
 
 app.use('/common', cmmRoutes);
+app.use("/file", fileUpRouter);
 app.use('/api', apiRoutes);
 app.use('/login', login);
 
--- backend/common/router/index.js
+++ backend/common/router/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cmmRouter = require('./api/common');
-const fileUpRouter = require('./api/file');
 const router = express.Router();
 
 router.use("/cmm", cmmRouter);
-router.use("/file", fileUpRouter);
 
 module.exports = router; 
