3b14b9dcccb8da72d8bf0ff74360a4b49b321a5f
강창수님/SKtelecom <P152670@sktelecom.com>
2020-10-12 15:56:06 +0900

    [20.10.12][강창수] clean code

--- backend/app.js
+++ backend/app.js
@@ -37,34 +37,3 @@ function normalizePort(val) {
     if (port >= 0) return port;
     return false;
 }
-
-
-
-
-
-
-
-/* bak
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-const router = require('./common/route/router');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
-*/
