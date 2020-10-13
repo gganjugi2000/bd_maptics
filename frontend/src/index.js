55fb25ed75b9b1fe4faccce865849204bb310b94
Jung-Hyo <javahyo@gmail.com>
2020-10-13 16:43:09 +0900

    init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import configure, { runSaga } from './store/configure';
+import App from './App';
+import './index.css';
 
+export const store = configure();
+runSaga();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
